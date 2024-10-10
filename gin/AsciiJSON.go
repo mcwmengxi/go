c41c19a3492eae5f50fa056f9868f58724f84569
mcwmengxi <1395568275@qq.com>
2024-10-10 23:38:38 +0800

    feat ✨: gin更新

--- gin/AsciiJSON.go
+++ gin/AsciiJSON.go
@@ -19,6 +19,29 @@ func main() {
 		c.AsciiJSON(http.StatusOK, data)
 	})
 
+	r.POST("/posts", func(c *gin.Context) {
+		id := c.Query("id")
+		ids := c.QueryMap("ids")
+		page := c.DefaultQuery("page", "0")
+		username := c.DefaultPostForm("username", "none")
+		password := c.DefaultPostForm("password", "000000") // 可设置默认值
+		names := c.PostFormMap("names")
+		// var cv map[string]interface{}
+		// c.BindJSON(cv)
+		c.JSON(http.StatusOK, gin.H{
+			"id":       id,
+			"ids":      ids,
+			"page":     page,
+			"username": username,
+			"password": password,
+			// "cv": cv,
+			"names": names,
+			"path":  c.FullPath(),
+		})
+	})
+	r.GET("/redirect", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/index")
+	})
 	// 监听并在 0.0.0.0:8080 上启动服务
 	r.Run(":8080")
 }
